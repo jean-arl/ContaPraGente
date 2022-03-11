f85903cb0bf0261049163fa929bd8f0d47de8b0b
Jean <jean_arl@hotmail.com>
2022-03-10 21:40:03 -0300

    Pagina Home

--- ContaPraGente/Src/Pages/Home/Home.js
+++ ContaPraGente/Src/Pages/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 
 export default function Home () {
     return (
         <ScrollView style={{ width:'100%' }}>
-            
+            <Text style={{textAlign: 'center' , textDecorationColor: 'Red', textTransform: 'uppercase'}}>Home</Text>
         </ScrollView>
     )
 }
