baa4597a33716a5d59423941bbee19d55610c8f8
Jean <jean_arl@hotmail.com>
2022-03-11 06:54:50 -0300

    atualização routes

--- ContaPraGente/Src/Pages/Home/Home.js
+++ ContaPraGente/Src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, ScrollView } from 'react-native';
 
-export default function Home () {
+export default function Perfil () {
     return (
         <ScrollView style={{ width:'100%' }}>
             <Text style={{textAlign: 'center' , textDecorationColor: 'Red', textTransform: 'uppercase'}}>Home</Text>
         </ScrollView>
+
     )
 }
